3e7e6d70f298ca512a5f13f6a843550e5aa4a281
Joshua Campos <jdeveloper1203@gmail.com>
2022-08-30 12:04:35 -0400

    commit tree 5cd59ad99eba969882920eb62f9f2ffbca623431

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(names){
+    const cards = [];
+    for (let i = 0; i < names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    };
+    return cards;
+};
+
+function countDown(num){
+    let n = 0;
+    n += num;
+
+    for (let count = 0; num > count; count++){
+        console.log(n);
+        n--;
+    }
+    console.log(n);
+};
